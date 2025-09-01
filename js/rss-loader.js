dd87c971ee924c39fe664637ac5dd16f27c0c4dc
hepemo106-sys <hepemo106@gmail.com>
2025-09-01 13:17:54 +0200

    Update rss-loader.js

--- js/rss-loader.js
+++ js/rss-loader.js
@@ -1,17 +1,3 @@
-const container = document.getElementById('rss-container');
-
-fetch('/api/feed')
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://www.eleconomista.es/rss/&api_key=TU_API_KEY')
   .then(res => res.json())
-  .then(data => {
-    data.forEach(item => {
-      const article = document.createElement('article');
-      article.className = 'mb-6 border border-green-600 p-4 rounded';
-      article.innerHTML = `
-        <h3 class='font-semibold text-green-200'>${item.title}</h3>
-        <p class='text-green-100 mt-2'>${new Date(item.pubDate).toLocaleDateString()}</p>
-        <a href='${item.link}' class='text-green-400 underline mt-2 inline-block' target='_blank'>Leer más</a>
-      `;
-      container.appendChild(article);
-    });
-  })
-  .catch(err => console.error('Error cargando noticias:', err));
+  .then(data => { /* mostrar noticias */ })
